d9c4d8e6d8fa0003f4d840fe5bdcb1f4a6d6db83
Ricardo Costa <racc.costa@gmail.com>
2018-05-06 21:19:14 -0300

    New theme

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createMuiTheme, MuiThemeProvider } from 'material-ui/styles';
-import { red } from 'material-ui/colors';
+import { blueGrey } from 'material-ui/colors';
 import registerServiceWorker from './registerServiceWorker';
 
 const theme = createMuiTheme({
     palette: {
-        primary: red
+        primary: blueGrey
     }
 })
 
